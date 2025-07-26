309a523473deb7cfb2ddd90e1530e698a2fadce7
Vikash Kumar <wiryvikash15@gmail.com>
2025-07-26 08:49:35 +0530

    Numeric Functions

---
+++ Numeric Functions.sql
@@ -0,0 +1,15 @@
+# Numeric Functions
+
+select abs(-36) as Absoulte_Value;
+
+select ceil(-7.65);
+
+select floor(-5.4);
+
+select round(23.43685282,2);
+
+select mod(32,9); #Remainder
+
+select power(5,2);
+
+select exp(3); #exponential value around 2.71
